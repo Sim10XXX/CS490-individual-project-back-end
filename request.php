e15d61628f77dcc8f223021ca4cde253e3d64a3b
Sim10XXX <75750668+Sim10XXX@users.noreply.github.com>
2023-09-28 00:32:15 -0400

    Add files via upload
    reportpdf uses classes from fpdf, but it doesn't work anyways so I'm not including it just yet

---
+++ request.php
@@ -0,0 +1,223 @@
+<?php
+
+    $q = $_REQUEST["q"];
+    $v = $_REQUEST["v"];
+    
+    $cnx = new mysqli('localhost', 'root', 'Msf56288!)', 'sakila');
+			
+    if ($cnx->connect_error)
+        die('Connection failed: ' . $cnx->connect_error);
+    switch ($q){
+        //Home page requests
+        //
+        //
+        case 2: //Details of movie
+            $query = '  SELECT title, release_year, description, length, rating, rental_rate
+                        FROM film
+                        WHERE title = "'. $v .'"
+            ';
+			$cursor = $cnx->query($query);
+            $row = $cursor->fetch_assoc();
+            
+            echo '<h3> '. $row['title'] .' ('. $row['release_year'] .') '. $row['length'] .' min, rated: '. $row['rating'] .'</h3><p>'. $row['description'] .'</p>';
+            break;
+        case 1: //Top 5 movies
+            $query = '  SELECT title, COUNT(*) AS rented
+                        FROM rental, inventory, film
+                        WHERE rental.inventory_id = inventory.inventory_id AND inventory.film_id = film.film_id
+                        GROUP BY title
+                        ORDER BY COUNT(*) DESC
+                        LIMIT 5
+            ';
+			$cursor = $cnx->query($query);
+
+            echo '<table><h3>';
+            echo "<tr><th>Movies</th><th># of rentals</th></tr>";
+			while ($row = $cursor->fetch_assoc()) {
+				echo '<tr><td> <button type="button" class="button2" onclick="subrequest(2, \''. $row['title'] .'\')">' . $row['title'] . ' </button></td><td> ' . $row['rented'] . ' </td></tr>';
+            }
+            echo '</h3></table>';
+            break;
+        
+        case 4: //Actor details
+            $query = '  SELECT first_name, last_name, title, COUNT(*) AS rented
+                        FROM rental, inventory, film, film_actor, actor
+                        WHERE rental.inventory_id = inventory.inventory_id AND inventory.film_id = film.film_id AND film_actor.film_id = film.film_id AND film_actor.actor_id = "'.$v.'" AND film_actor.actor_id = actor.actor_id
+                        GROUP BY title
+                        ORDER BY COUNT(*) DESC
+                        LIMIT 5
+            ';
+			$cursor = $cnx->query($query);
+            $row = $cursor->fetch_assoc();
+            echo '<h3>Top 5 Movies for '. $row['first_name'] . ' ' . $row['last_name'] .'</h3>';
+            echo '<table><h3>';
+            echo "<tr><th>Movies</th><th># of rentals</th></tr>";
+            do{
+                echo '<tr><td> <button type="button" class="button2" onclick="subrequest(2, \''. $row['title'] .'\')">' . $row['title'] . ' </button></td><td> ' . $row['rented'] . ' </td></tr>';
+            }while($row = $cursor->fetch_assoc());
+            echo '</h3></table>';
+            break;
+        case 3: //Top 5 actors
+            $query = '  SELECT actor.actor_id, first_name, last_name, COUNT(*) AS movies
+                        FROM film, film_actor, actor
+                        WHERE film.film_id = film_actor.film_id AND film_actor.actor_id = actor.actor_id
+                        GROUP BY actor_id
+                        ORDER BY COUNT(*) DESC
+                        LIMIT 5
+            ';
+			$cursor = $cnx->query($query);
+            echo '<table><h3>';
+            echo "<tr><th>Actor</th><th># of movies</th></tr>";
+			while ($row = $cursor->fetch_assoc()) {
+				echo '<tr><td><button type="button" class="button2" onclick="subrequest(4, \''. $row['actor_id'] .'\')"> ' . $row['first_name'] . ' ' . $row['last_name'] . ' </button></td><td> ' . $row['movies'] . ' </td></tr>';
+            }
+            echo '</h3></table>';
+            break;
+
+        //Report page requests
+        //
+        //
+        case 5: //download pdf
+
+            header("Location: reportpdf.php"); /* Redirect browser */
+
+            /* Make sure that code below does not get executed when we redirect. */
+            exit;
+
+            break;
+
+        //Movie page requests
+        //
+        //
+        case 6: //Search movie list by title or actor
+            $g = $_REQUEST["g"];
+            if ($g == 'Any'){
+                $query = '  SELECT title
+                            FROM film, film_actor, actor
+                            WHERE film.film_id = film_actor.film_id AND film_actor.actor_id = actor.actor_id AND 
+                            (actor.first_name LIKE "%'. $v .'%" OR title LIKE "%'. $v .'%")
+            ';
+            }
+            else{
+                $query = '  SELECT title
+                            FROM film, film_actor, actor, film_category, category
+                            WHERE film.film_id = film_actor.film_id AND film_actor.actor_id = actor.actor_id AND 
+                            (actor.first_name LIKE "%'. $v .'%" OR title LIKE "%'. $v .'%") AND 
+                            film.film_id = film_category.film_id AND film_category.category_id = category.category_id
+                            AND category.name = "'. $g .'"
+            ';
+            }
+            
+            $cursor = $cnx->query($query);
+            echo '<table><h3>';
+            echo "<tr><th>Movie Title</th></tr>";
+            while($row = $cursor->fetch_assoc()){
+                echo '<tr><td> <button type="button" class="button2" onclick="subrequest(2, \''. $row['title'] .'\')">' . $row['title'] . ' </button></td></tr>';
+            }
+            echo '</h3></table>';
+            break;
+
+        //Customer page requests
+        //
+        //
+        case 7: //List of customers depending on filtered on user input
+            $x = $_REQUEST["x"];
+            $y = $_REQUEST["y"];
+            $z = $_REQUEST["z"];
+
+            $query = '  SELECT customer_id, first_name, last_name
+                        FROM customer
+                        WHERE customer_id LIKE "%'. $x .'%" AND first_name LIKE "%'. $y .'%" AND last_name LIKE "%'. $z .'%"
+            ';
+            $cursor = $cnx->query($query);
+            echo '<table><h3>';
+            echo "<tr><th>Customer ID</th><th>First Name</th><th>Last Name</th></tr>";
+            while($row = $cursor->fetch_assoc()){
+                echo '<tr><td> <button type="button" class="button2" onclick="subrequest(8, \''. $row['customer_id'] .'\')">' . $row['customer_id'] . ' </button></td>
+                <td>' . $row['first_name'] . '</td>
+                <td>' . $row['last_name'] . '</td></tr>';
+            }
+            echo '</h3></table>';
+            break;
+        case 8: //Customer details and edit capability
+            $query = '  SELECT customer_id, first_name, last_name, email, active, create_date, country, city, address, address2, district, postal_code, phone
+                        FROM customer, address, city, country
+                        WHERE customer.address_id = address.address_id AND address.city_id = city.city_id AND city.country_id = country.country_id
+                        AND customer_id = "'. $v .'"
+            ';
+            $cursor = $cnx->query($query);
+            $row = $cursor->fetch_assoc();
+            echo '<form action="update.php?q=1&id='. $row['customer_id'] .'" method="post">
+            <p>Customer ID: '. $row['customer_id'] .'<br>Create Date: "'. $row['create_date'] .'"</p>
+            First Name: <input type="text" name = "first_name" id= "first_name" value = "' . $row['first_name'] . '"><br>
+             Last Name: <input type="text" name = "last_name" id= "last_name" value = "' . $row['last_name'] . '"><br>
+             Email: <input type="text" name = "email" id= "email" value = "' . $row['email'] . '"><br>
+             Active: <input type="text" name = "active" id= "active" value = "' . $row['active'] . '"><br>
+             Country: <input type="text" name = "country" id= "country" value = "' . $row['country'] . '">
+             City: <input type="text" name = "city" id= "city" value = "' . $row['city'] . '"><br>
+             Address: <input type="text" name = "address" id= "address" value = "' . $row['address'] . '">
+             Address 2: <input type="text" name = "address2" id= "address2" value = "' . $row['address2'] . '"><br>
+             District: <input type="text" name = "district" id= "district" value = "' . $row['district'] . '">
+             Postal Code: <input type="text" name = "postal_code" id= "postal_code" value = "' . $row['postal_code'] . '"><br>
+             Phone: <input type="text" name = "phone" id= "phone" value = "' . $row['phone'] . '"><br>
+             <input type="submit" value="Update Values">
+             <br><br><br><br><br>
+        </form>
+        
+        <form action="update.php?q=2&id='. $row['customer_id'] .'" method="post">
+            <input type="submit" value="Delete Customer">
+        </form>
+        ';
+            
+            
+            $query = '  SELECT title, rental_date, return_date
+                        FROM rental, customer, inventory, film
+                        WHERE inventory.inventory_id = rental.inventory_id AND inventory.film_id = film.film_id
+                        AND rental.customer_id = customer.customer_id AND customer.customer_id = "'. $v .'"
+            ';
+            $cursor = $cnx->query($query);
+            echo'<h3>Rented Movies</h3>';
+            echo '<table><h3>';
+            echo "<tr><th>Movie Tile</th><th>Rental Date</th><th>Return Date</th></tr>";
+            while($row = $cursor->fetch_assoc()){
+                echo '<tr><td>' . $row['title'] . '</td>
+                <td>' . $row['rental_date'] . '</td>
+                <td>' . $row['return_date'] . '</td></tr>';
+            }
+            echo '</h3></table>';
+            break;
+        case 9: //Add new customer
+            echo '<form action="update.php?q=0" method="post">
+            First Name: <input type="text" id= "first_name" name= "first_name" ><br>
+             Last Name: <input type="text" id= "last_name" name= "last_name" ><br>
+             Email: <input type="text" id= "email" name= "email" ><br>
+             Active: <input type="text" id= "active" name= "active" ><br>
+             Country: <input type="text" id= "country" name= "country" >
+             City: <input type="text" id= "city" name= "city" ><br>
+             Address: <input type="text" id= "address" name= "address" >
+             Address 2: <input type="text" id= "address2" name= "address2" ><br>
+             District: <input type="text" id= "district" name= "district" >
+             Postal Code: <input type="text" id= "postal_code" name= "postal_code" ><br>
+             Phone: <input type="text" id= "phone" name= "phone" ><br>
+
+             <input type="submit" value="Create New Customer">
+        </form>';
+            break;
+        case -1: //test case
+            $query = '  SELECT country
+                        FROM country
+                        WHERE country = "Algeri"
+            ';
+            $cursor = $cnx->query($query);
+            $row = $cursor->fetch_assoc();
+            if ($row != NULL){
+                echo "not null";
+            }
+            else{
+                echo "null";
+            }
+            break;
+    }
+
+    //echo $v;
+?> 
---
+++ update.php
@@ -0,0 +1,154 @@
+<?php
+$q = $_REQUEST["q"]; 
+//q==0: create new customer
+//q==1: update customer values
+//q==2: delete customer
+echo "yep";
+//echo $_POST["email"];
+$cnx = new mysqli('localhost', 'root', 'Msf56288!)', 'sakila');
+			
+if ($cnx->connect_error)
+    die('Connection failed: ' . $cnx->connect_error);
+
+switch ($q){
+    case 0: //q==0: create new customer
+        //Check country
+        $query = '  SELECT country_id
+                    FROM country
+                    WHERE country = "'.$_POST["country"].'"
+        ';
+        $cursor = $cnx->query($query);
+        $row = $cursor->fetch_assoc();
+        if ($row == NULL){
+            $query = 'INSERT INTO country (country)
+                        VALUES ("'.$_POST["country"].'")
+            ';
+            $cursor = $cnx->query($query);
+            $last_id = $cnx->insert_id;
+        }
+        else{
+            $last_id = $row["country_id"];
+        }
+        //Check city
+        $query = '  SELECT city_id
+                    FROM city
+                    WHERE city = "'.$_POST["city"].'"
+        ';
+        $cursor = $cnx->query($query);
+        $row = $cursor->fetch_assoc();
+        
+        if ($row == NULL){
+            $query = '  INSERT INTO city (city, country_id)
+                        VALUES ("'.$_POST["city"].'", "'.$last_id.'")
+            ';
+            $cursor = $cnx->query($query);
+            $last_id = $cnx->insert_id;
+        }
+        else{
+            $last_id = $row["city_id"];
+        }
+        //Check Address
+        $query = '  SELECT address_id
+                    FROM address
+                    WHERE address = "'.$_POST["address"].'" AND district = "'.$_POST["district"].'" AND phone = "'.$_POST["phone"].'"
+        ';
+        $cursor = $cnx->query($query);
+        $row = $cursor->fetch_assoc();
+        
+        if ($row == NULL){
+            // !!! location is currently always (0, 0) !!!
+            $query = '  INSERT INTO address (address, address2, district, city_id, postal_code, phone, location)
+                        VALUES ("'.$_POST["address"].'", "'.$_POST["address2"].'", "'.$_POST["district"].'", "'.$last_id.'", "'.$_POST["postal_code"].'", "'.$_POST["phone"].'", POINT(0,0))
+            ';
+            $cursor = $cnx->query($query);
+            $last_id = $cnx->insert_id;
+        }
+        else{
+            $last_id = $row["address_id"];
+        }
+
+        //Insert Customer
+
+        // !!! store id is currently always 1 !!!
+        $query = '  INSERT INTO customer (first_name, last_name, email, store_id, address_id)
+                    VALUES ("'.$_POST["first_name"].'", "'.$_POST["last_name"].'", "'.$_POST["email"].'", 1, "'.$last_id.'")
+        ';
+        $cursor = $cnx->query($query);
+        break;
+    case 1: //q==1: update customer values
+        $id = $_REQUEST["id"]; 
+        $query = '  UPDATE customer
+                    SET first_name = "'.$_POST["first_name"].'", last_name = "'.$_POST["last_name"].'", email = "'.$_POST["email"].'", active = "'.$_POST["active"].'"
+                    WHERE customer_id = '.$id.' 
+        ';
+        $cursor = $cnx->query($query);
+
+        //Check country
+        $query = '  SELECT country_id
+                    FROM country
+                    WHERE country = "'.$_POST["country"].'"
+        ';
+        $cursor = $cnx->query($query);
+        $row = $cursor->fetch_assoc();
+        if ($row == NULL){
+            $query = 'INSERT INTO country (country)
+                        VALUES ("'.$_POST["country"].'")
+            ';
+            $cursor = $cnx->query($query);
+            $last_id = $cnx->insert_id;
+        }
+        else{
+            $last_id = $row["country_id"];
+        }
+        //Check city
+        $query = '  SELECT city_id
+                    FROM city
+                    WHERE city = "'.$_POST["city"].'"
+        ';
+        $cursor = $cnx->query($query);
+        $row = $cursor->fetch_assoc();
+        
+        if ($row == NULL){
+            $query = '  INSERT INTO city (city, country_id)
+                        VALUES ("'.$_POST["city"].'", "'.$last_id.'")
+            ';
+            $cursor = $cnx->query($query);
+            $last_id = $cnx->insert_id;
+        }
+        else{
+            $last_id = $row["city_id"];
+        }
+        //Check Address
+        $query = '  SELECT address_id
+                    FROM address
+                    WHERE address = "'.$_POST["address"].'" AND district = "'.$_POST["district"].'" AND phone = "'.$_POST["phone"].'"
+        ';
+        $cursor = $cnx->query($query);
+        $row = $cursor->fetch_assoc();
+        
+        if ($row == NULL){
+            // !!! location is currently always (0, 0) !!!
+            $query = '  INSERT INTO address (address, address2, district, city_id, postal_code, phone, location)
+                        VALUES ("'.$_POST["address"].'", "'.$_POST["address2"].'", "'.$_POST["district"].'", "'.$last_id.'", "'.$_POST["postal_code"].'", "'.$_POST["phone"].'", POINT(0,0))
+            ';
+            $cursor = $cnx->query($query);
+            $last_id = $cnx->insert_id;
+
+
+            $query = '  UPDATE customer
+                        SET address_id = '.$last_id.'
+                        WHERE customer_id = '.$id.' 
+            ';
+            $cursor = $cnx->query($query);
+        }
+        break;
+    case 2: //q==2: delete customer
+        // !!! this will only work with test customer, because dependecies aren't taken care of !!!
+        $id = $_REQUEST["id"]; 
+        $query = '  DELETE FROM customer 
+                    WHERE customer_id = '.$id.';
+        ';
+        $cursor = $cnx->query($query);
+        break;
+}  
+?>
